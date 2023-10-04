23cd2519710d2051cd89470c97b12e599dc13c47
AbdulRahmanJainun22A <146951521+AbdulRahmanJainun22A@users.noreply.github.com>
2023-10-04 20:41:51 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+// Mendefinisikan variabel
+var firstName = "Abdul Rahman"
+var lastName = "Jainun";
+var age = 19; 
+var isMarried = false; // Ganti dengan status pernikahan Anda (true/false)
+
+// Contoh penggunaan variabel
+console.log("Nama: " + firstName + " " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + (isMarried ? "Menikah" : "Belum Menikah"));
